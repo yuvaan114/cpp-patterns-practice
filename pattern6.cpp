50cb8ff5c22d941270b8f1e13f7176dbc7444705
yuvaan114 <yuvaan.0412@gmail.com>
2025-07-03 16:49:41 -0700

    Added pattern 6

--- pattern6.cpp
+++ pattern6.cpp
@@ -2,8 +2,8 @@
 using namespace std;
 int main(){
     int n = 4;
-    for (int i = 1; i <= n; i++){
-        for(int j = 1; j<= i; j++){
+    for (int i = 0; i < n; i++){
+        for(int j = 0; j < i; j++){
             cout<<"*";
         }
         cout<<endl;
