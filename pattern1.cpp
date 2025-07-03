874561efaedfd15c4333958d5ecf8870b46da6a9
yuvaan114 <yuvaan.0412@gmail.com>
2025-07-03 16:17:05 -0700

    Added pattern1

---
+++ pattern1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n = 4;
+    for(int i = 0; i<n; i++){
+        for(int j = 1; j<=n; j++){
+            cout<<j;
+        }
+        cout<<endl;
+
+    }
+    return 0;
+}
